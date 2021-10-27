95bbee7310f0984ece916c372a122a38301930f9
Mona Moghaddas <moghaddas.mona@gmail.com>
2021-10-26 21:30:05 -0700

    clear code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import Item from '../src/components/Item'
-import TextField from '../src/components/TextField'
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
